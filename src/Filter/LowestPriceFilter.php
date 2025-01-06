2c87fb5799bd589a6d61017a7aac6efafef591b8
Rafał Borowski <moderator9781@gmail.com>
2025-01-06 17:23:56 +0100

    Identify further steps - make mock pass the test

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -9,7 +9,22 @@ class LowestPriceFilter implements PromotionsFilterInterface
 {
   public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
   {
-    $enquiry->setDiscountedPrice(50);
+    $price = $enquiry->getProduct()->getPrice();
+    $quantity = $enquiry->getQuantity();
+    $lowestPrice = $price * $quantity;
+
+    //Loop over the promotions
+      // Run the promotions' modification logic
+      // 1. check does the promotion apply e.g. is it in the date range/is the voucher valid
+      // 2. Apply the price modification to obtain a $modifiedPrice (how?)
+//      $modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
+
+      // 3. check if $modifiedPrice < $lowestPrice
+        // 1. save to Enquiry properties
+        // 3. Update $lowestPrice
+
+
+    $enquiry->setDiscountedPrice(250);
     $enquiry->setPrice(100);
     $enquiry->setPromotionId(3);
     $enquiry->setPromotionName('Black Friday half price sale');
