b6fc13f830681e842c7fafe58b561105c2cb6633
Rafał Borowski <moderator9781@gmail.com>
2025-01-11 00:01:13 +0100

    Add Validator package and use it in event

--- src/EventSubscriber/DtoSubscriber.php
+++ src/EventSubscriber/DtoSubscriber.php
@@ -4,9 +4,16 @@ namespace App\EventSubscriber;
 
 use App\Event\AfterDtoCreatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class DtoSubscriber implements EventSubscriberInterface
 {
+
+  public function __construct(private readonly ValidatorInterface $validator)
+  {
+  }
+
   public static function getSubscribedEvents(): array
   {
     return [
@@ -16,6 +23,12 @@ class DtoSubscriber implements EventSubscriberInterface
 
   public function validateDto(AfterDtoCreatedEvent $event): void
   {
-    // Validate the dto
+    $dto = $event->getDto();
+
+    $errors = $this->validator->validate($dto);
+    if (count($errors) > 0) {
+        throw new ValidationFailedException('Validation failed', $errors);
+    }
+
   }
 }
